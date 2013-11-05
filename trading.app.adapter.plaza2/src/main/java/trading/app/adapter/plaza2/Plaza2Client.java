705aa9f1cb9d4e28ffd815ef766469263c2892c7
dima <dmitry.pukhov@gmail.com>
2013-11-05 04:36:42 +0400

    CGate only console logging left, no file.

--- trading.app.adapter.plaza2/src/main/java/trading/app/adapter/plaza2/Plaza2Client.java
+++ trading.app.adapter.plaza2/src/main/java/trading/app/adapter/plaza2/Plaza2Client.java
@@ -32,7 +32,9 @@ public class Plaza2Client {
 		});
 
 		try {
-			CGate.open("ini=jsend.ini;key=11111111");
+			//CGate.open("ini=./ini/jsend.ini;key=Plaza2Client");
+			
+			CGate.open("key=Plaza2Client"); // no log
 			connection = new Connection(
 					"p2tcp://127.0.0.1:4001;app_name=plaza2_client");
 			// listener = new Listener(connection,
