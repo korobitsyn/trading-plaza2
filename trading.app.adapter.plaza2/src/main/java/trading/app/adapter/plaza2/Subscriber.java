312bd4966f5cc8b3dbc6e679ac3f94f9db608a3e
dima <dmitry.pukhov@gmail.com>
2013-11-05 04:45:29 +0400

    Simple switch added to plaza message subscriber.

--- trading.app.adapter.plaza2/src/main/java/trading/app/adapter/plaza2/Subscriber.java
+++ trading.app.adapter.plaza2/src/main/java/trading/app/adapter/plaza2/Subscriber.java
@@ -4,6 +4,8 @@ import ru.micexrts.cgate.Connection;
 import ru.micexrts.cgate.ErrorCode;
 import ru.micexrts.cgate.ISubscriber;
 import ru.micexrts.cgate.Listener;
+import ru.micexrts.cgate.MessageType;
+import ru.micexrts.cgate.messages.DataMessage;
 import ru.micexrts.cgate.messages.Message;
 
 /**
@@ -17,12 +19,25 @@ public class Subscriber implements ISubscriber {
 	 * Process new message
 	 */
 	public int onMessage(Connection conn, Listener listener, Message message) {
-		
-		System.out.println(message);
+		// Process by message type
+		switch (message.getType()) {
+		// Data message
+		case MessageType.MSG_DATA:
+			DataMessage msgData = (DataMessage) message;
+			System.out.println(String.format(
+					"Reply message. ID=%d, User-Id=%d, content=%s",
+					msgData.getMsgId(), msgData.getUserId(), message));
+			break;
+		// Timeout
+		case MessageType.MSG_P2MQ_TIMEOUT:
+			System.out.println("Timeout message");
+			break;
+		// Default	
+		default:
+			System.out.println(message);
+		}
+
 		return ErrorCode.OK;
 	}
-
-
-
 
 }
