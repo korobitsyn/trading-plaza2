ba4fd9bdc5070b9559eeff6dbd15873bb0bd40e1
pdg <pdg@dpuhov.msk.mt>
2013-11-14 17:22:42 +0400

    Spring DI added to get adapter

--- trading.app/src/main/java/trading/app/TradingApplication1.java
+++ trading.app/src/main/java/trading/app/TradingApplication1.java
@@ -7,8 +7,9 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.List;
 
+import org.springframework.context.support.GenericXmlApplicationContext;
+
 import trading.app.adapter.plaza2.Adapter;
-import trading.app.adapter.plaza2.Plaza2Adapter;
 import trading.app.provider.HistoryProvider;
 import trading.data.model.Level1;
 
@@ -18,11 +19,24 @@ import trading.data.model.Level1;
  *
  */
 public class TradingApplication1 {
+	// Spring application context
+	static GenericXmlApplicationContext ctx;
+	
+	private Adapter adapter;
+	
+	public Adapter getAdapter() {
+		return adapter;
+	}
+
+	public void setAdapter(Adapter adapter) {
+		this.adapter = adapter;
+	}
+
 	/**
 	 * Get history from plaza 2
 	 * @param adapter
 	 */
-	protected static void getHistory(Adapter adapter){
+	protected void readHistory(Adapter adapter){
 		HistoryProvider historyProvider = new HistoryProvider();
 		Calendar startCal = GregorianCalendar.getInstance();
 		startCal.set(Calendar.YEAR, 2010);
@@ -35,6 +49,7 @@ public class TradingApplication1 {
 		Date endTime = endCal.getTime();
 
 		List<Level1> range = historyProvider.getLevel1Range(startTime, endTime);		
+		System.out.println(String.format("Found %d items", range.size()) );
 	}
 	
 	/**
@@ -42,7 +57,7 @@ public class TradingApplication1 {
 	 * @param adapter
 	 * @throws IOException 
 	 */
-	protected static void writeHistory(Adapter adapter) throws IOException{
+	protected void writeHistory(Adapter adapter) throws IOException{
 		HistoryWriter historyWriter = new HistoryWriter(adapter);
 		adapter.connect();
 		System.out.println("Press any key to finish");
@@ -61,10 +76,23 @@ public class TradingApplication1 {
 	 */
 	public static void main(String[] args) throws
 			InterruptedException, IOException {
-		// Create adapter
-		System.out.println("Creating adapter");
-		Adapter adapter = new Plaza2Adapter();
-		writeHistory(adapter);
+		ctx = new GenericXmlApplicationContext();
+		ctx.load("classpath:META-INF/spring/application-context.xml");
+		ctx.registerShutdownHook();
+		ctx.refresh();
+		TradingApplication1 app = ctx.getBean(TradingApplication1.class);
+		app.run();	
+
 		
 	}	
+	
+	/**
+	 * Main entrance
+	 * @throws IOException 
+	 */
+	public void run() throws IOException{
+	
+		
+		readHistory(adapter);
+	}
 }
