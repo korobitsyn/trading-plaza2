765cc0b9c2f856ff3fd53c3258813c1b591d2b53
pdg <pdg@dpuhov.msk.mt>
2013-11-15 12:14:00 +0400

    Connects to market

--- trading.app.adapter.plaza2/src/main/java/trading/app/adapter/plaza2/Plaza2Client.java
+++ trading.app.adapter.plaza2/src/main/java/trading/app/adapter/plaza2/Plaza2Client.java
@@ -48,8 +48,8 @@ public class Plaza2Client {
 		});
 
 		try {
-			// CGate.open("key=11111111");
-			CGate.open("ini=ini/jsend.ini;key=11111111");
+			 CGate.open("key=11111111");
+			//CGate.open("ini=ini/jsend.ini;key=11111111");
 			connection = new Connection(
 					"p2tcp://127.0.0.1:4001;app_name=plaza2_client");
 
