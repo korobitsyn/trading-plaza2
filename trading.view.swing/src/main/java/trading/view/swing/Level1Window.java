0c5e6aaefb90f649d5442a68c5c6dde35e979b79
pdg <pdg@dpuhov.msk.mt>
2013-11-15 18:06:16 +0400

    trading.app.common renamed to trading.common

--- trading.view.swing/src/main/java/trading/view/swing/Level1Window.java
+++ trading.view.swing/src/main/java/trading/view/swing/Level1Window.java
@@ -5,7 +5,23 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import java.util.ResourceBundle;
 import java.awt.Toolkit;
+import javax.swing.JSplitPane;
+import java.awt.BorderLayout;
+import javax.swing.SpringLayout;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import javax.swing.JToolBar;
+import javax.swing.JComboBox;
+import javax.swing.JToggleButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
+/**
+ * Application window for Level1 chart
+ * @author pdg
+ *
+ */
 public class Level1Window {
 
 	private JFrame frame;
@@ -42,6 +58,20 @@ public class Level1Window {
 		frame.setTitle(ResourceBundle.getBundle("trading.view.swing.messages").getString("Level1WindowTitle")); //$NON-NLS-1$ //$NON-NLS-2$
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		JToolBar toolBar = new JToolBar();
+		frame.getContentPane().add(toolBar, BorderLayout.NORTH);
+		
+		JComboBox comboBox = new JComboBox();
+		toolBar.add(comboBox);
+		
+		JToggleButton captureDataButton = new JToggleButton(ResourceBundle.getBundle("trading.view.swing.messages").getString("Level1Window.captureButton.text")); //$NON-NLS-1$ //$NON-NLS-2$
+		captureDataButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+		});
+		toolBar.add(captureDataButton);
 	}
 
 }
