c383c0dbf498d42079614f2a6baffbb74bf0efbc
dima <dmitry.pukhov@gmail.com>
2013-11-23 10:56:07 +0400

    Level1 loader test refactored.

--- trading.app.neural/src/test/java/trading/app/neural/test/Level1LoaderTest.java
+++ trading.app.neural/src/test/java/trading/app/neural/test/Level1LoaderTest.java
@@ -70,28 +70,14 @@ public class Level1LoaderTest extends AbstractTest {
 			setAsk(new BigDecimal(1));
 			setAskSize(1);
 		}};
-		// Get normalized data to compare
-		double nDate = ReflectionTestUtils.invokeMethod(loader, "getNormalizedDate", level1.getDate());
-		double nPrice = ReflectionTestUtils.invokeMethod(loader, "getNormalizedPrice", level1.getLastPrice(), prevLevel1.getLastPrice());
-		double nSize = ReflectionTestUtils.invokeMethod(loader, "getNormalizedSize", level1.getLastSize(), prevLevel1.getLastSize());
-		double nBid = ReflectionTestUtils.invokeMethod(loader, "getNormalizedPrice", level1.getBid(), prevLevel1.getBid());
-		double nBidSize = ReflectionTestUtils.invokeMethod(loader, "getNormalizedSize", level1.getBidSize(), prevLevel1.getBidSize());
-		double nAsk = ReflectionTestUtils.invokeMethod(loader, "getNormalizedPrice", level1.getAsk(), prevLevel1.getAsk());
-		double nAskSize = ReflectionTestUtils.invokeMethod(loader, "getNormalizedSize", level1.getAskSize(), prevLevel1.getAskSize());
 		
 		// Call and check the result
 		int pos = 0;
 		Integer resultPos = ReflectionTestUtils.invokeMethod(loader, "addLevel1Data", mlData, pos, level1, prevLevel1);
 		assertEquals(singleLevel1Size, resultPos);
-		assertEquals(nDate, mlData.getData(0), Constants.DOUBLE_COMPARISON_PRECISION);
-		assertEquals(nPrice, mlData.getData(1),Constants.DOUBLE_COMPARISON_PRECISION);
-		assertEquals(nSize, mlData.getData(2),Constants.DOUBLE_COMPARISON_PRECISION);
-		assertEquals(nBid, mlData.getData(3),Constants.DOUBLE_COMPARISON_PRECISION);
-		assertEquals(nBidSize, mlData.getData(4),Constants.DOUBLE_COMPARISON_PRECISION);
-		assertEquals(nAsk, mlData.getData(5),Constants.DOUBLE_COMPARISON_PRECISION);
-		assertEquals(nAskSize, mlData.getData(6), Constants.DOUBLE_COMPARISON_PRECISION);
-		assertEquals(0, mlData.getData(7), Constants.DOUBLE_COMPARISON_PRECISION);
-		assertEquals(0, mlData.getData(8), Constants.DOUBLE_COMPARISON_PRECISION);
+		assertMLDataContainsLevel1(loader, mlData, pos, level1, prevLevel1);
+		assertEquals(0.0, mlData.getData(singleLevel1Size), Constants.DOUBLE_COMPARISON_PRECISION);
+		assertEquals(0.0, mlData.getData(singleLevel1Size+1), Constants.DOUBLE_COMPARISON_PRECISION);
 		
 		mlData = new BasicMLData(singleLevel1Size +2);
 		// Call and check the result
@@ -100,15 +86,8 @@ public class Level1LoaderTest extends AbstractTest {
 		Integer idealPos = pos + singleLevel1Size;
 		assertEquals(idealPos, resultPos);
 		assertEquals(0, mlData.getData(0), Constants.DOUBLE_COMPARISON_PRECISION);
-		assertEquals(nDate, mlData.getData(pos + 0), Constants.DOUBLE_COMPARISON_PRECISION);
-		assertEquals(nPrice, mlData.getData(pos + 1),Constants.DOUBLE_COMPARISON_PRECISION);
-		assertEquals(nSize, mlData.getData(pos + 2),Constants.DOUBLE_COMPARISON_PRECISION);
-		assertEquals(nBid, mlData.getData(pos + 3),Constants.DOUBLE_COMPARISON_PRECISION);
-		assertEquals(nBidSize, mlData.getData(pos + 4),Constants.DOUBLE_COMPARISON_PRECISION);
-		assertEquals(nAsk, mlData.getData(pos + 5),Constants.DOUBLE_COMPARISON_PRECISION);
-		assertEquals(nAskSize, mlData.getData(pos + 6), Constants.DOUBLE_COMPARISON_PRECISION);
-		assertEquals(0, mlData.getData(8), Constants.DOUBLE_COMPARISON_PRECISION);
-		assertEquals(0, mlData.getData(resultPos), Constants.DOUBLE_COMPARISON_PRECISION);
+		assertMLDataContainsLevel1(loader, mlData, pos, level1, prevLevel1);
+		assertEquals(0, mlData.getData(singleLevel1Size+1), Constants.DOUBLE_COMPARISON_PRECISION);
 		
 	}
 	/**
@@ -156,6 +135,36 @@ public class Level1LoaderTest extends AbstractTest {
 		double expected = new Double(curValue - prevValue) / new Double(prevValue);
 		result = ReflectionTestUtils.invokeMethod(loader, "getNormalizedSize", curValue, prevValue);
 		assertEquals(expected, result, Constants.DOUBLE_COMPARISON_PRECISION);
+		
+	}
+	
+	/**
+	 * Check whether mldata contains level1
+	 * @param loader
+	 * @param mlData
+	 * @param index level1 data index in mldata
+	 * @param level1
+	 * @param prevLevel1
+	 */
+	private void assertMLDataContainsLevel1(Level1Loader loader, MLData mlData, int index, Level1 level1, Level1 prevLevel1){
+
+		// Get normalized data to compare
+		double nDate = ReflectionTestUtils.invokeMethod(loader, "getNormalizedDate", level1.getDate());
+		double nPrice = ReflectionTestUtils.invokeMethod(loader, "getNormalizedPrice", level1.getLastPrice(), prevLevel1.getLastPrice());
+		double nSize = ReflectionTestUtils.invokeMethod(loader, "getNormalizedSize", level1.getLastSize(), prevLevel1.getLastSize());
+		double nBid = ReflectionTestUtils.invokeMethod(loader, "getNormalizedPrice", level1.getBid(), prevLevel1.getBid());
+		double nBidSize = ReflectionTestUtils.invokeMethod(loader, "getNormalizedSize", level1.getBidSize(), prevLevel1.getBidSize());
+		double nAsk = ReflectionTestUtils.invokeMethod(loader, "getNormalizedPrice", level1.getAsk(), prevLevel1.getAsk());
+		double nAskSize = ReflectionTestUtils.invokeMethod(loader, "getNormalizedSize", level1.getAskSize(), prevLevel1.getAskSize());
+
+		assertEquals(nDate, mlData.getData(index + 0), Constants.DOUBLE_COMPARISON_PRECISION);
+		assertEquals(nPrice, mlData.getData(index + 1),Constants.DOUBLE_COMPARISON_PRECISION);
+		assertEquals(nSize, mlData.getData(index + 2),Constants.DOUBLE_COMPARISON_PRECISION);
+		assertEquals(nBid, mlData.getData(index + 3),Constants.DOUBLE_COMPARISON_PRECISION);
+		assertEquals(nBidSize, mlData.getData(index + 4),Constants.DOUBLE_COMPARISON_PRECISION);
+		assertEquals(nAsk, mlData.getData(index + 5),Constants.DOUBLE_COMPARISON_PRECISION);
+		assertEquals(nAskSize, mlData.getData(index + 6), Constants.DOUBLE_COMPARISON_PRECISION);
+		assertEquals(0, mlData.getData(8), Constants.DOUBLE_COMPARISON_PRECISION);
 		
 	}
 
